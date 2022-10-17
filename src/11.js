39d4c7888daf14e057295130cfb41de0eae061ad
alex5667 <alex56671111@gmail.com>
2022-10-17 14:27:32 +0300

    feat: add what

--- src/11.js
+++ src/11.js
@@ -1,11 +1,33 @@
-function createDreamTeam(members) {
-    if (members == null) return false
-    return Array.from(members).reduce((acc, curr) => {
-        if (typeof curr === 'string') {
-            curr = curr.replace(/^ +| +$|( ) +/g, "$1")
-            acc = acc + curr[0];
-        }
-        return acc
-    }, '').toUpperCase().split('').sort().join('')
+function getSeason(date) {
+    if (date == null) return 'Unable to determine the time of year!'
+    if (date.hasOwnProperty('toString')) throw new Error('Invalid date!')
+    let month = (date.getMonth() + 1).toString();
+  
+    let season = '';
+    switch (month) {
+      case '12':
+      case '1':
+      case '2':
+        season = 'winter';
+        break;
+      case '3':
+      case '4':
+      case '5':
+        season = 'spring';
+        break;
+      case '6':
+      case '7':
+      case '8':
+        season = 'summer';
+        break;
+      case '9':
+      case '10':
+      case '11':
+        season = 'fall';
+        break;
+    }
+    return season;
+
 }
-console.log(createDreamTeam(null))
+
+console.log(getSeason(new Date(2020, 02, 31)))
